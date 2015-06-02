7af886d2483c90290ca7c8aca56209950a2d9e1e
Tim Spencer <tspencer@vowhs.com>
2015-06-01 23:50:10 -0400

    Tim Spencer

---
+++ CashRegister/Denomination.cs
@@ -0,0 +1,73 @@
+﻿namespace CashRegister
+{
+    /// <summary>
+    /// Class that defines denominations of money to be used to give change
+    /// </summary>
+    public class Denomination
+    {
+        private int _count = 0;
+        private readonly int _value;
+        private readonly string _stringForOne;
+        private readonly string _stringForMore;
+        private string _display;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="value">The value of this denomination in cents</param>
+        /// <param name="stringForOne">The label for one of this denomination</param>
+        /// <param name="stringForMore">The label for two or more of this denomination</param>
+        public Denomination(int value, string stringForOne, string stringForMore)
+        {
+            _value = value;
+            _stringForOne = stringForOne;
+            _stringForMore = stringForMore;
+            _display = stringForMore;
+        }
+
+        /// <summary>
+        /// The count of this denomination contained in the change
+        /// </summary>
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                _display = value == 1 ? StringForOne : StringForMore;
+            }
+        }
+
+        /// <summary>
+        /// The value of this denomination in cents
+        /// </summary>
+        public int Value
+        {
+            get { return _value; }
+        }
+
+        /// <summary>
+        /// The label for one of this denomination
+        /// </summary>
+        public string StringForOne
+        {
+            get { return _stringForOne; }
+        }
+
+        /// <summary>
+        /// The label for two or more of this denomination
+        /// </summary>
+        public string StringForMore
+        {
+            get { return _stringForMore; }
+        }
+
+        /// <summary>
+        /// The display value for the count and label of this denomination contained in the change
+        /// </summary>
+        public string Display
+        {
+            get { return string.Format("{0} {1}", _count, _display); }
+        }
+    }
+}
---
+++ CashRegister/TransactionFile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CashRegister
+{
+    /// <summary>
+    /// This static class processes a cash register transaction file
+    /// </summary>
+    public static class TransactionFile
+    {
+        /// <summary>
+        /// Gets the transactions from the batch transaction file
+        /// </summary>
+        /// <param name="streamReader">The StreamReader for the comma delimitted input file 
+        /// with one transaction per line.</param>
+        /// <returns>a list of Transactions extracted from the file</returns>
+        public static List<Transaction> GetTransactions(StreamReader streamReader)
+        {
+            var results = new List<Transaction>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                bool isBaddata = true;
+                var values = line.Split(',');
+                if (values.Count() == 2)
+                {
+                    double charged;
+                    double tendered;
+                    if (double.TryParse(values[0], out charged) && NumberOfDecimalPlaces(values[0]) == 2 &&
+                        double.TryParse(values[1], out tendered) && NumberOfDecimalPlaces(values[1]) == 2)
+                    {
+                        var transaction = new Transaction { Charged = charged, Tendered = tendered };
+                        results.Add(transaction);
+                        isBaddata = false;
+                    }
+                }
+                if (isBaddata)
+                {
+                    throw new InvalidDataException("The transaction data file is not in the correct format.");
+                }
+            }
+
+            return results;
+        }
+
+        private static int NumberOfDecimalPlaces(string numberString)
+        {
+            var parts = numberString.Split('.');
+            return parts.Count() == 2 ? parts[1].Length : 0;
+        }
+    }
+}
