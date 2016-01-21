8d6705ee2115a43b364d5223ad462d68d93ad673
jchadman <jchadman@gmail.com>
2016-01-21 10:45:30 -0500

    Joe Chadman Creative Cash Draw

---
+++ JoeChadman-CreativeCashDraw/JoeChadman-CreativeCashDraw/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using JoeChadman_CreativeCashDraw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CsvHelper.Configuration;
+using System.IO;
+using CsvHelper;
+
+namespace JoeChadman_CreativeCashDraw.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Cash Register";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            List<Register> register = new List<Register>();
+
+            //get data from csv
+            try {
+                if (file.ContentLength > 0)
+                {
+                    IEnumerable<Register> records;
+                    using (var reader = new CsvReader(new StreamReader(file.FileName)))
+                    { 
+                        //no header record.  This property is trure by default.
+                        reader.Configuration.HasHeaderRecord = false;
+
+                        //use a mapping file to pull the data into the class correctly by index
+                        reader.Configuration.RegisterClassMap<RegisterClassMapping>();
+
+                        //Fill Class from comma delimited file
+                        records = reader.GetRecords<Register>();
+
+                        //need to have the records in a list so converting from the ienumberable csvhelper needed
+                        register = records.ToList();
+                    }                    
+                }
+            }
+            catch (Exception ex) {
+                ViewBag.Error = "Upload Failed: " + ex.Message;
+            }
+            
+            return View(register);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Joe Chadman";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Joe Chadman";
+
+            return View();
+        }
+    }
+}
---
+++ JoeChadman-CreativeCashDraw/JoeChadman-CreativeCashDraw/Models/Register.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CsvHelper;
+
+namespace JoeChadman_CreativeCashDraw.Models
+{
+    public class Register
+    {
+        public decimal AmtOwed { get; set; }
+        public decimal AmtPaid { get; set; }
+
+        public bool RandomChange {
+            get { return (this.AmtOwed * 100 % 3 == 0) ? true : false; }
+        }
+
+        public decimal AmtChange
+        {
+            get { return this.AmtPaid - this.AmtOwed; }
+        }
+
+        public string Change { 
+            get {
+                return getChange();
+            }
+        }
+
+
+        private string getChange()
+        {
+            string returnChange = null;
+            int cents = (int)(Math.Round(this.AmtChange, 2) * 100);
+
+            var currency = new[] { 
+                new { NameSingle = "Twenty Dollar Bill", NamePlural = "Twenty Dollar Bills", Value = 2000 }, new { NameSingle = "Ten Dollar Bill", NamePlural = "Ten Dollar Bills", Value = 1000 }, 
+                new { NameSingle = "Five Dollar Bill", NamePlural = "Five Dollar Bills", Value = 500 }, new { NameSingle = "One Dollar Bill", NamePlural = "One Dollar Bills", Value = 100 }, 
+                new { NameSingle = "Quarter", NamePlural = "Quarters", Value = 25 }, new { NameSingle = "Dime", NamePlural = "Dimes", Value = 10 }, 
+                new { NameSingle = "Nickel", NamePlural = "Nickels", Value = 5 }, new { NameSingle = "Penny", NamePlural = "Pennies", Value = 1 } };
+
+
+            //calculate change needed
+            if(cents < 0)
+            {
+                //Not enough money tendered
+                returnChange = "$" + this.AmtChange * -1 + " still needed to pay off amount owed";
+            }
+            else if (cents == 0)
+            {
+                //No change needed
+                returnChange =  "No changed";
+            }
+            else if (cents > 0)
+            {
+                if (!this.RandomChange)
+                {
+                    var change = currency.Select(coin => new { Amt = Math.DivRem(cents, coin.Value, out cents), Coin = coin })
+                                .Where(x => x.Amt != 0).ToList();
+
+                    change.ForEach(x =>
+                            returnChange += x.Amt > 1 ? x.Amt + " " + x.Coin.NamePlural + ", " : x.Amt + " " + x.Coin.NameSingle + ", "
+                        );
+                }
+                else
+                {
+                    //returnChange = "TO DO: Calcualte Random Change";
+                    while (cents > 0)
+                    {
+                        //pick a random currency
+                        var wipCurrency = currency[new Random().Next(currency.Length)];
+
+                        //make sure randum currency is not larger than change needed
+                        if (wipCurrency.Value < cents)
+                        {
+                            //how much of selected currency to use
+                            double wipNumCurrency = cents / wipCurrency.Value;
+                            wipNumCurrency = Math.Floor(wipNumCurrency);
+
+
+                            returnChange += wipNumCurrency > 1 ? wipNumCurrency + " " + wipCurrency.NamePlural + ", " : wipNumCurrency + " " + wipCurrency.NameSingle + ", ";
+
+                            cents -= (int)wipNumCurrency * wipCurrency.Value;
+                        }
+                    }
+                }
+
+                //format return change string
+                returnChange = returnChange.Substring(0, returnChange.Length - 2);
+            }
+           
+            
+            return returnChange;
+        }
+
+    }
+}
