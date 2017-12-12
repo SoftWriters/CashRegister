9e180e154fbe61c85eb9d38b5bb4cc1eba42079c
lgettel <lgettel@zoll.com>
2017-12-12 18:20:18 -0500

    Code Challange

---
+++ CashRegister/ChangeTranslator/Dtos/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeTranslator.Dtos
+{
+    public class Transaction
+    {
+        public decimal Cost { get; }
+        public decimal Paid { get; }
+        public bool RandomChange => (Cost * 100) % 3 == 0;
+
+        public Transaction(string cost, string paid)
+        {
+            if (decimal.TryParse(cost, out decimal temp))
+                Cost = temp;
+            else throw new InvalidCastException($"{cost} cannot be cast as a decimal.");
+
+            if (decimal.TryParse(paid, out temp))
+                Paid = temp;
+            else throw new InvalidCastException($"{paid} cannot be cast as a decimal.");
+        }
+    }
+}
---
+++ CashRegister/ChangeTranslator/Dtos/UnitedStatesCurrency.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ChangeTranslator.Dtos
+{
+    public class UnitedStatesCurrency : ICurrency
+    {
+        public IEnumerable<Denomination> Denominations { get; }
+        public string NoChangePhrase { get; }
+
+        public UnitedStatesCurrency()
+        {
+            Denominations = new List<Denomination>
+            {
+                new Denomination {SingularName = "dollar", PluralName = "dollars", Value = 1m},
+                new Denomination {SingularName = "quarter", PluralName = "quarters", Value = .25m},
+                new Denomination {SingularName = "dime", PluralName = "dimes", Value = .10m},
+                new Denomination {SingularName = "nickel", PluralName = "nickels", Value = .05m},
+                new Denomination {SingularName = "penny", PluralName = "pennies", Value = .01m}
+            };
+
+            NoChangePhrase = "No change";
+        }
+    }
+}
