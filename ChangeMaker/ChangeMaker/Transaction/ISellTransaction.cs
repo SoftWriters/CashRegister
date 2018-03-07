d60e866dd6ef188d45bc3c1ab1cf1b7f02009b96
marcp3558 <31511573+marcp3558@users.noreply.github.com>
2018-03-07 01:14:19 -0500

    Added interfaces

---
+++ ChangeMaker/ChangeMaker/Transaction/ISellTransaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    public interface ISellTransaction : ITransaction
+    {
+        void CalculateChange();
+        void DisplayChange();
+    }
+}
