4249768b190469080567acf7da5960938128209f
marcp3558 <31511573+marcp3558@users.noreply.github.com>
2018-03-07 00:21:33 -0500

    Added Log

---
+++ ChangeMaker/ChangeMaker/Log.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    public static class Log
+    {
+        public static List<string> LogEntries;
+
+        public static void Initialize()
+        {
+            LogEntries = new List<string>();
+        }
+
+        public static void WriteLine(string msg)
+        {
+            Console.WriteLine(msg);
+            LogEntries.Add(msg);
+        }
+
+        public static void WriteLine()
+        {
+            WriteLine("");
+        }
+
+        public static void OutputToFile(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach(var entry in LogEntries)
+                {
+                    writer.WriteLine(entry);
+                }
+            }
+        }
+    }
+}
