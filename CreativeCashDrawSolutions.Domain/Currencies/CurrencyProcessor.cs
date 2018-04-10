6518a462338660670189a61e340353e2d81718bd
jh-sw <38134092+jh-sw@users.noreply.github.com>
2018-04-10 11:46:32 -0400

    Adding some XML comments to the processor class.
    No logic changes were added to this change.

--- CreativeCashDrawSolutions.Domain/Currencies/CurrencyProcessor.cs
+++ CreativeCashDrawSolutions.Domain/Currencies/CurrencyProcessor.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using CreativeCashDrawSolutions.Entities.Exceptions;
 using CreativeCashDrawSolutions.Entities.Helpers;
 
 namespace CreativeCashDrawSolutions.Domain.Currencies
 {
+    /// <summary>This class is responsible for defining how to process a currency in the library.</summary>
     public abstract class CurrencyProcessor
     {
         private readonly CurrencyType _type;
         private readonly int[] _coins;
 
+        /// <summary>Specialised constructor for use only by derived class.</summary>
+        /// <param name="type">The currency type to be used by the processor.</param>
         protected CurrencyProcessor(CurrencyType type)
         {
             _type = type;
             _coins = _type.GetDenominationTypes().Select(x => x.Value).ToArray();
         }
 
+        /// <summary>This routine is reponsible for taking the defined pattern for an input string and providing the appropriate output.</summary>
+        /// <param name="totalAndPaid">The value owed and paid seperated by a comma [x.xx, y.yy].</param>
+        /// <returns>A string description of the denominations that are needed to make correct change.</returns>
         public string GetOutputString(string totalAndPaid)
         {
             int due, paid;
@@ -24,6 +30,10 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
             return InputStringHelper.ShouldBeRandom(totalAndPaid) ? GetOutputString(difference, true) : GetOutputString(difference);
         }
 
+        /// <summary>This routine is responsible for ordering, grouping and counting the denominations.</summary>
+        /// <param name="total">The total amount of change that is due to the caller.</param>
+        /// <param name="randomize">true if the denominations should be randomized.</param>
+        /// <returns>A collection of the denomination amount along with the count of items in each.</returns>
         private Dictionary<int, int> GetChangeSolution(int total, bool randomize = false)
         {
             var solution = EvaluateDenomination(total, randomize);
@@ -41,6 +51,15 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
             return result.ToDictionary(x => x.Currency, x => x.Total);
         }
 
+        /// <summary>
+        /// This routine is reponsible for taking the amount due to the caller and providing the
+        /// appropriate output.
+        /// </summary>
+        /// <param name="total">The total amount of change that is due to the caller.</param>
+        /// <param name="random">true if the denominations should be randomized.</param>
+        /// <returns>
+        /// A string description of the denominations that are needed to make correct change.
+        /// </returns>
         private string GetOutputString(int total, bool random = false)
         {
             var strings = new List<string>();
@@ -52,6 +71,11 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
             return string.Join(",", strings);
         }
 
+        /// <summary>This routine is responsible for getting a grid of all possible solutions for owed amount and denominations supported.</summary>
+        /// <param name="solutions">A reference to the solution grid.</param>
+        /// <param name="currentSum">The total amount of change that needs to be evaluated.</param>
+        /// <param name="minCoin">The minimum coin.</param>
+        /// <param name="commmaSeperatedList">Running commma seperated string of coins to return.</param>
         private void GetEntireGrid(ICollection<List<int>> solutions, int currentSum, int minCoin, string commmaSeperatedList)
         {
             // TODO: This needs to be optimized, this could take a considerable amount of time
@@ -80,9 +104,22 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
             }
         }
 
+        /// <summary>
+        /// This routine is responsible for evaluating which type of calculation needs to be performed on
+        /// the data.
+        /// </summary>
+        /// <exception cref="NoPossibleSolutionException">
+        /// Thrown when no possible solution can be found.
+        /// </exception>
+        /// <param name="total">The total amount of change that is due to the caller.</param>
+        /// <param name="random">true if the denominations should be randomized.</param>
+        /// <returns>
+        /// An enumerator that allows foreach to be used to process evaluate denomination in this
+        /// collection.
+        /// </returns>
         private IEnumerable<int> EvaluateDenomination(int total, bool random = false)
         {
-            // Taking a slow route by calculating all the possibilities and grabbing a random one out of it O(n < 20) solutions do not randomize very well since the 
+            // Taking a slow route by calculating all the possibilities and grabbing a random one out of it O(n < 20) solutions do not randomize very well
             if (random)
             {
                 var solutionGrid = new List<List<int>>();
@@ -110,6 +147,7 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
                 coinPlaceholder[i] = -1;
             }
 
+            // Evaluate using DP technique
             for (var j = 0; j < _coins.Length; j++)
             {
                 for (var i = 1; i <= total; i++)
