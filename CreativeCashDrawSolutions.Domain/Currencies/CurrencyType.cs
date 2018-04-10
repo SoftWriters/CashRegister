904903913734df83e6943582abf3d12c0a5cf30f
jh-sw <38134092+jh-sw@users.noreply.github.com>
2018-04-10 11:47:45 -0400

    Added some XML comments to the CurrencyType class.
    No logic changes were made to this class.

--- CreativeCashDrawSolutions.Domain/Currencies/CurrencyType.cs
+++ CreativeCashDrawSolutions.Domain/Currencies/CurrencyType.cs
@@ -1,19 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using CreativeCashDrawSolutions.Entities;
 
 namespace CreativeCashDrawSolutions.Domain.Currencies
 {
+    /// <summary>This class is responsible for defining the type of currency in the library.</summary>
     public abstract class CurrencyType
     {
+        /// <summary>Gets the denominations of the implemented currency.</summary>
+        /// <value>A collection of supported denominations.</value>
         protected abstract IEnumerable<DenominationType> Denominations { get; }
 
+        /// <summary>Gets the denomination types in this collection.</summary>
+        /// <returns>
+        /// An enumerator that allows foreach to be used to process the denomination types in this
+        /// collection.
+        /// </returns>
         public IEnumerable<DenominationType> GetDenominationTypes()
         {
             return Denominations.OrderByDescending(x => x.Value);
         }
 
+        /// <summary>This routine is responsible for getting the correct output string based on the denomination value and count.</summary>
+        /// <param name="value">The denomination value to evaluate.</param>
+        /// <param name="count">Number of denominations to evaluate.</param>
+        /// <returns>A singular or plural string containing the output.</returns>
+        /// <remarks>If the output of 1 penny or 2 pennies is not correct for the currency code, you can override it to support the currency.</remarks>
         public virtual string GetOutputStringByValue(int value, int count)
         {
             var type = GetTypeByValue(value);
@@ -22,6 +35,12 @@ namespace CreativeCashDrawSolutions.Domain.Currencies
             return string.Format("{0} {1}", count, count > 1 ? type.NamePlural : type.NameSingular);
         }
 
+        /// <summary>This routine is responsible for getting the type of currency based on the value.</summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown in the event that a denomination is passed in that is not supported in the currency.
+        /// </exception>
+        /// <param name="value">The denomination value to evaluate.</param>
+        /// <returns>The denomination type that maps to the value.</returns>
         protected DenominationType GetTypeByValue(int value)
         {
             var type = Denominations.Select(x => x).FirstOrDefault(x => x.Value == value);
