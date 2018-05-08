7eb7b39a931e8c82f7aa100bc89770791924039c
Anton Yakymenko <antonyak@pm.me>
2018-05-08 19:48:11 -0400

    Added CashRegisterControllerTest

---
+++ CashRegister.Tests/Controllers/CashRegisterControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using CashRegister.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace CashRegister.Tests.Controllers
+{
+    [TestClass]
+    public class CashRegisterControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            CashRegisterController controller = new CashRegisterController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
