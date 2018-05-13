bf4cab0cd8d9da91cfff6d5f5d81fbd1876c9a13
Tony Frazier <afrazier@traumazone.com>
2018-05-12 21:05:03 -0400

    Add transaction objects

---
+++ CashRegister/ITransaction.cs
@@ -0,0 +1,9 @@
+﻿namespace CashRegister
+{
+    public interface ITransaction
+    {
+        decimal MoneyPaid { get; }
+
+        decimal MoneyOwed { get; }
+    }
+}
---
+++ CashRegister/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace CashRegister
+{
+    public class Transaction : ITransaction
+    {
+        public decimal MoneyOwed { get; }
+
+        public decimal MoneyPaid { get; }
+
+        public decimal MoneyDue => MoneyPaid - MoneyOwed;
+
+        public Transaction(decimal moneyOwed, decimal moneyPaid)
+        {
+            if (moneyOwed > moneyPaid)
+                throw new InvalidTransactionException("Money paid must be greater than or equal to money owed.");
+
+            MoneyOwed = moneyOwed;
+            MoneyPaid = moneyPaid;
+        }
+    }
+}
