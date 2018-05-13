2a74701d7036baadc16591e9c7fff3387d8226dc
Tony Frazier <afrazier@traumazone.com>
2018-05-12 20:34:49 -0400

    Add command line options and scaffold parser

---
+++ CashRegister/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace CashRegister
+{
+    internal class Options
+    {
+        [Option('i', Default = "-", Required = false, HelpText = "Name of the input file.  Use '-' for stdin.")]
+        public string InFile { get; set; }
+
+        [Option('o', Default = "-", Required = false, HelpText = "Name of the output file.  Use '-' for stdout.")]
+        public string OutFile { get; set; }
+    }
+}
--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace CashRegister
 {
@@ -10,6 +11,9 @@ namespace CashRegister
     {
         public static void Main(string[] args)
         {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => { })
+                .WithNotParsed(e => Environment.ExitCode = 1);
         }
     }
 }
