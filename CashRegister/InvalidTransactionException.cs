9f1e28cb7c71c8fb330be570157e2b2d783f2d38
Tony Frazier <afrazier@traumazone.com>
2018-05-14 02:08:45 -0400

    Update InvalidTransactionException

--- CashRegister/InvalidTransactionException.cs
+++ CashRegister/InvalidTransactionException.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashRegister
 {
@@ -11,5 +7,7 @@ namespace CashRegister
         public InvalidTransactionException(string message) : base(message) { }
 
         public InvalidTransactionException(string message, Exception innerException) : base(message, innerException) { }
+
+        public InvalidTransactionException() { }
     }
 }
