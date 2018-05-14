bbe37ab7cba53c9707bb43e2f0827711de88f979
Tony Frazier <afrazier@traumazone.com>
2018-05-14 02:09:05 -0400

    Reuse ChangeFormatters

--- CashRegister/ChangeFormatterProvider.cs
+++ CashRegister/ChangeFormatterProvider.cs
@@ -1,12 +1,17 @@
-﻿using CashRegister.ChangeFormatters;
+﻿using System;
+using CashRegister.ChangeFormatters;
 
 namespace CashRegister
 {
     public static class ChangeFormatterProvider
     {
+        private static Lazy<IChangeFormatter> GreedyChangeFormatter => new Lazy<IChangeFormatter>(() => new GreedyChangeFormatter());
+
+        private static Lazy<IChangeFormatter> RandomChangeFormatter => new Lazy<IChangeFormatter>(() => new RandomChangeFormatter());
+
         public static IChangeFormatter GetChangeFormatter(Transaction transaction) =>
             (transaction.ChangeDue % 0.03m) == 0m
-                ? (IChangeFormatter)new RandomChangeFormatter()
-                : new GreedyChangeFormatter();
+                ? RandomChangeFormatter.Value
+                : GreedyChangeFormatter.Value;
     }
 }
