1c75c1e14058a2c3d4cb8516225b6747f27adcf4
Tony Frazier <afrazier@traumazone.com>
2018-05-14 02:09:25 -0400

    Fill in main program

--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -1,19 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using CommandLine;
+using CashRegister.ChangeFormatters;
+using CashRegister.TransactionReaders;
 
 namespace CashRegister
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             CommandLine.Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(o => { })
-                .WithNotParsed(e => Environment.ExitCode = 1);
+                .WithParsed(RunProgram)
+                .WithNotParsed(errors => Environment.ExitCode = 1);
+
+            return Environment.ExitCode;
+        }
+
+        private static void RunProgram(Options options)
+        {
+            try
+            {
+                var useStdIn = options.InFile == "-";
+                var useStdOut = options.OutFile == "-";
+                TextReader inStream = null;
+                TextWriter outStream = null;
+                try
+                {
+                    inStream = useStdIn ? Console.In : new StreamReader(options.InFile);
+                    outStream = useStdOut ? Console.Out : new StreamWriter(options.OutFile);
+
+                    ITransactionReader transactionReader = new SimpleFileTransactionReader(inStream);
+
+                    foreach (Transaction transaction in transactionReader.GetTransactions())
+                    {
+                        IChangeFormatter changeFormatter = ChangeFormatterProvider.GetChangeFormatter(transaction);
+                        outStream.WriteLine(changeFormatter.FormatChangeResult(transaction));
+                    }
+                }
+                finally
+                {
+                    if (!useStdIn)
+                        inStream?.Dispose();
+                    if (!useStdOut)
+                        outStream?.Dispose();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+                Environment.ExitCode = 2;
+            }
         }
     }
 }
