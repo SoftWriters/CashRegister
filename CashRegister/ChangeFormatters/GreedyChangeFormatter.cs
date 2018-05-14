00ca216be20fed8bf5bd94408d709a2f64c7b4ce
Tony Frazier <afrazier@traumazone.com>
2018-05-14 02:51:36 -0400

    Implement ChangeFormatters

--- CashRegister/ChangeFormatters/GreedyChangeFormatter.cs
+++ CashRegister/ChangeFormatters/GreedyChangeFormatter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CashRegister.ChangeFormatters
 {
@@ -10,7 +6,57 @@ namespace CashRegister.ChangeFormatters
     {
         public string FormatChangeResult(Transaction transaction)
         {
-            return string.Empty;
+            if (transaction.ChangeDue == 0m)
+                return "No change due";
+
+            decimal remainingChange = transaction.ChangeDue;
+
+            var result = new List<string>();
+
+            if (remainingChange >= 1.0m)
+            {
+                var dollars = (int)(remainingChange / 1.0m);
+                if (dollars > 1)
+                    result.Add($"{dollars} dollars");
+                else
+                    result.Add("1 dollar");
+                remainingChange -= dollars;
+            }
+            if (remainingChange >= 0.25m)
+            {
+                var quarters = (int)(remainingChange / 0.25m);
+                if (quarters > 1)
+                    result.Add($"{quarters} quarters");
+                else
+                    result.Add("1 quarter");
+                remainingChange -= quarters * 0.25m;
+            }
+            if (remainingChange >= 0.10m)
+            {
+                var dimes = (int)(remainingChange / 0.10m);
+                if (dimes > 1)
+                    result.Add($"{dimes} dimes");
+                else
+                    result.Add("1 dime");
+                remainingChange -= dimes * 0.10m;
+            }
+            if (remainingChange >= 0.05m)
+            {
+                var nickels = (int)(remainingChange / 0.05m);
+                if (nickels > 1)
+                    result.Add($"{nickels} nickels");
+                else
+                    result.Add("1 nickel");
+                remainingChange -= nickels * 0.05m;
+            }
+
+            var pennies = (int)(remainingChange * 100);
+            if (pennies > 1)
+                result.Add($"{pennies} pennies");
+            else if (pennies == 1)
+                result.Add("1 penny");
+
+            return string.Join(",", result);
         }
     }
 }
--- CashRegister/ChangeFormatters/RandomChangeFormatter.cs
+++ CashRegister/ChangeFormatters/RandomChangeFormatter.cs
@@ -1,16 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashRegister.ChangeFormatters
 {
     public class RandomChangeFormatter : IChangeFormatter
     {
+        private readonly Random random = new Random();
+
         public string FormatChangeResult(Transaction transaction)
         {
-            return string.Empty;
+            if (transaction.ChangeDue == 0m)
+                return "No change due";
+
+            decimal remainingChange = transaction.ChangeDue;
+
+            var result = new List<string>();
+
+            if (remainingChange >= 1.0m)
+            {
+                var dollars = random.Next((int)(remainingChange / 1.0m) + 1);
+                if (dollars > 1)
+                    result.Add($"{dollars} dollars");
+                else if (dollars == 1)
+                    result.Add("1 dollar");
+                remainingChange -= dollars;
+            }
+            if (remainingChange >= 0.25m)
+            {
+                var quarters = random.Next((int)(remainingChange / 0.25m) + 1);
+                if (quarters > 1)
+                    result.Add($"{quarters} quarters");
+                else if (quarters == 1)
+                    result.Add("1 quarter");
+                remainingChange -= quarters * 0.25m;
+            }
+            if (remainingChange >= 0.10m)
+            {
+                var dimes = random.Next((int)(remainingChange / 0.10m) + 1);
+                if (dimes > 1)
+                    result.Add($"{dimes} dimes");
+                else if (dimes == 1)
+                    result.Add("1 dime");
+                remainingChange -= dimes * 0.10m;
+            }
+            if (remainingChange >= 0.05m)
+            {
+                var nickels = random.Next((int)(remainingChange / 0.05m) + 1);
+                if (nickels > 1)
+                    result.Add($"{nickels} nickels");
+                else if (nickels == 1)
+                    result.Add("1 nickel");
+                remainingChange -= nickels * 0.05m;
+            }
+
+            var pennies = (int)(remainingChange * 100);
+            if (pennies > 1)
+                result.Add($"{pennies} pennies");
+            else if (pennies == 1)
+                result.Add("1 penny");
+
+            return string.Join(",", result);
         }
     }
 }
