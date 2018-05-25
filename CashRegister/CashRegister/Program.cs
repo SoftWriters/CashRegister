aca9cea38bb678fb0c1527eeaebd769befb4d295
LAUERJM1 <jonmlauer@gmail.com>
2018-05-25 12:29:18 -0400

    Initial commit with auto-generated code from C# console project.

---
+++ CashRegister/CashRegister/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Cockadoodledoo");
+            System.Console.ReadLine();
+        }
+    }
+}
