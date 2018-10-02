2dbbe75d121daeec91f7ccd4f8a39ef2b4dafda9
Philip Washy <p.t.washy@gmail.com>
2018-10-02 17:05:11 -0400

    Currency Interface
    Created interface to allow the swapping of currencies depending on register locaton.

---
+++ Currency.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Cash;
+
+namespace Cash {
+    public interface Currency {
+        Denomination get_closest_denomination(double value);
+        Denomination get_denomination( int index );
+    }
+}
