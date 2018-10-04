5dee7f9a480eddbe8319790c5f1ff5f30601b52d
Philip Washy <p.t.washy@gmail.com>
2018-10-04 00:24:15 -0400

    Consolidated Code
    Moved a copy of each piece of code written into a single folder outside of the Visual Studio project for ease of access.

---
+++ Code/Change.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cash;
+
+namespace Cash
+{
+    public class Change: Denomination
+    {
+        public int count { get; set; }
+
+        public Change(double v, String n, String p)
+        {
+            count  = 0;
+            value  = v;
+            name   = n;
+            plural = p;
+        }
+
+        public Change(double v, String n, String p, int c)
+        {
+            count = c;
+            value = v;
+            name = n;
+            plural = p;
+        }
+    }
+}
