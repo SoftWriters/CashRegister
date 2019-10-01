88e76efa0cd11f7510b0757ba714fc4049640b54
Jason Henriquez <jasonthomashenriquez@gmail.com>
2019-10-01 00:21:09 -0400

    Add files via upload

---
+++ CCDS.CashRegister/CCDS.CashRegister/CashRegister.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCDS.CashRegister
+{
+    class CashRegister
+    {
+        public List<Transaction> Transactions { get; private set; } = new List<Transaction>();
+        private Transaction _lastTransaction;
+        private long overpay => _lastTransaction.Payment - _lastTransaction.Cost;
+        private bool _isCostDivisibleByThree;
+        private long[] _currency;
+
+        public CashRegister(long[] currency)
+        {
+            _currency = currency;
+        }
+
+        //stores previous transactions
+        public void AddTransaction(long cost, long payment)
+        {
+            Transaction exchange = new Transaction(cost, payment);
+            _isCostDivisibleByThree = cost % 3 == 0;
+            _lastTransaction = exchange;
+            Transactions.Add(exchange);
+        }
+
+        //returns change (random if divisible by 3) given the overpay of the transaction
+        public long[] GiveChange()
+        {
+            return (_isCostDivisibleByThree) ? ChangeDistributor.DistributeRandomDenominations(overpay, _currency) :
+                ChangeDistributor.DistributeNormalDenominations(overpay, _currency);
+        }
+
+        //checks to see if values are valid
+        public string TransactionErrorMessage(long cost, long payment)
+        {
+            if (cost < 0)
+            {
+                return "Error: invalid cost amount!";
+            }
+
+            else if (payment < 0)
+            {
+                return "Error: invalid payment amount!";
+            }
+
+            else if (cost > payment)
+            {
+                return "Error: payment must be greater than or equal to cost.";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CCDS.CashRegister/CCDS.CashRegister/ChangeDistributor.cs
@@ -0,0 +1,49 @@
+﻿//distributes change given an amount and currency
+
+using IdentityModel;
+using System;
+using System.Collections.Generic;
+
+namespace CCDS.CashRegister
+{
+    public static class ChangeDistributor
+    {
+        public static long[] DistributeRandomDenominations(long overpay, long[] currency)
+        {
+            long[] change = new long[currency.Length];
+            CryptoRandom rng = new CryptoRandom();
+            while (overpay > 0)
+            {
+                //creates random currency that is <= overpay
+                int randomNumber;
+                do
+                {
+                    randomNumber = rng.Next(0, currency.Length);
+                } while (currency[randomNumber] > overpay);
+
+                //removes currency amount and increments the number of that currency in the change
+                overpay -= currency[randomNumber];
+                ++change[randomNumber];
+            }
+
+            return change;
+        }
+
+        public static long[] DistributeNormalDenominations(long overpay, long[] currency)
+        {
+            var change = new long[currency.Length];
+
+            for (int i = 0; i < currency.Length; ++i)
+            {
+                while (overpay >= currency[i])
+                {
+                    //removes currency amount and increments the number of that currency in the change
+                    overpay -= currency[i];
+                    ++change[i];
+                }
+            }
+
+            return change;
+        }
+    }
+}
