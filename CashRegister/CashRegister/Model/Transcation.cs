6caa5f15b8aecec2edb5c33e19098e634722f427
arjunyar <58668688+arjunyar@users.noreply.github.com>
2019-12-11 14:05:19 -0500

    Arjun Yarlagadda's Cash Register Code
    Completed the code challenge of Cash Register.

---
+++ CashRegister/CashRegister/Model/Transcation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister.Model
+{
+    public class TranscationModel
+    {
+        public int Payment { get; set; }
+        public int Purchase { get; set; }
+    }
+}
