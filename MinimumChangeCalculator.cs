a64f6222f16b3a3cb654d652a14291a2d5fb3417
jfowler <jfowler_80@hotmail.com>
2019-12-27 11:46:40 -0500

    Add items to solution.

---
+++ MinimumChangeCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCashRegister
+{
+    public class MinimumChangeCalculator : IChangeCalculator
+    {
+        public IList<(Currency, int)> GetChange(int changeAmount)
+        {
+            List<(Currency Currency, int Quantity)> change = new List<(Currency Currency, int Quantity)>();
+
+            foreach (var currency in Currency.List())
+            {
+                if (changeAmount >= currency.Value)
+                {
+                    int quantity = changeAmount / currency.Value;
+                    change.Add((Currency: currency, Quantity: quantity));
+                    changeAmount -= currency.Value * quantity;
+                }
+            }
+
+            return change;
+        }
+    }
+}
