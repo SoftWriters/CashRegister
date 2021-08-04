c5063e4d60bd3e0b859297f06b2cf1546f09c5b9
Jon Fleet <jmfleet1@gmail.com>
2021-08-03 22:41:10 -0400

    Added Base Read/Write File Functionality

---
+++ CashRegister/ReadService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CashRegister
+{
+    public class ReadService
+    {
+        private static string FileName { get; } = "input.txt";
+        private static string Directory { get; } = Environment.CurrentDirectory;
+        private readonly string FilePath = Path.Combine(Directory, FileName);
+
+        public List<decimal> ReadFile()
+        {
+            List<decimal> output = new List<decimal>();
+
+            try
+            {
+                using(StreamReader streamReader = new StreamReader(FilePath))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string line = streamReader.ReadLine();
+                        string[] temp = new string[2];
+                        temp = line.Split(',');
+
+                        output.Add(decimal.Parse(temp[0]));
+                        output.Add(decimal.Parse(temp[1]));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            return output;
+        }
+    }
+}
---
+++ CashRegister/WriteService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CashRegister
+{
+    public class WriteService
+    {
+        private static string FileName { get; } = "output.txt";
+        private static string Directory { get; } = Environment.CurrentDirectory;
+        private readonly string FilePath = Path.Combine(Directory, FileName);
+
+        public void WriteFile(Change change)
+        {
+            try
+            {
+                using(StreamWriter streamWriter = new StreamWriter(FilePath, true))
+                {
+                    streamWriter.WriteLine($"{change.Dollar } dollar {change.Quarter} quarters {change.Dime} dimes ");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
