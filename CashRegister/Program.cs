1fc4b463c456b678674efccd28813aadf101e6c3
Jon Fleet <jmfleet1@gmail.com>
2021-08-05 19:22:48 -0400

    Connected Project Parts together

--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -12,10 +12,18 @@ namespace CashRegister
             ReadService reader = new ReadService();
             WriteService writer = new WriteService();
 
-            List<decimal> ouput = reader.ReadFile();
+            List<decimal> output = reader.ReadFile();
+
+            List<Change> changeList = new List<Change>();
+
+            for (int i = 0; i < output.Count - 1; i += 2)
+            {
+                Change change = new Change();
+                change = cr.GetChange(output[i], output[i + 1]);
+                changeList.Add(change);
+            }
             
-            Change change = cr.GetChange(3.33M);
-            writer.WriteFile(change);
+            writer.WriteFile(changeList);
 
         }
     }
