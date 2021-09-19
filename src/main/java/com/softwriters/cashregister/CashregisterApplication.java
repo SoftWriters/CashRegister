35900a1c46bdcc5dc62374e477459e06b1391c99
rmurano13 <robert.murano13@gmail.com>
2021-09-19 14:22:59 -0400

    Add project generated files

---
+++ src/main/java/com/softwriters/cashregister/CashregisterApplication.java
@@ -0,0 +1,13 @@
+package com.softwriters.cashregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashregisterApplication.class, args);
+	}
+
+}
