043e75e1ea1b8888fa5eadc06484e01195abf94f
rmurano13 <robert.murano13@gmail.com>
2021-09-19 22:14:53 -0400

    Add comments to change processing service

--- src/main/java/com/softwriters/cashregister/service/ChangeProcessingService.java
+++ src/main/java/com/softwriters/cashregister/service/ChangeProcessingService.java
@@ -20,20 +20,22 @@ public class ChangeProcessingService {
 	public String calculateChange(BigDecimal totalDue, BigDecimal amountGiven) {
 		
 		String change = "";
-		
+		//Determine change needed
 		BigDecimal changeNeeded = amountGiven.subtract(totalDue);
 		
 		for(int i=0;i<changeValues.length;i++) {
 			
 			int numberOfChange = 0;
-					
+			
+			//iterate from highest change value to lowest and subtract till less than zero or zero		
 			while((changeNeeded.subtract(changeValues[i]).compareTo(zero)==1)||
 					(changeNeeded.subtract(changeValues[i]).compareTo(zero)==0)) {
 				
+				//track number of change denominations and subtract from total change required
 				numberOfChange++;
 				changeNeeded = changeNeeded.subtract(changeValues[i]);
 			}
-			
+			//create strings for change based on number of change denominations
 			if(numberOfChange == 1) {
 				change += numberOfChange + " " + changeStringSingle[i] + ",";
 			}
@@ -50,20 +52,26 @@ public class ChangeProcessingService {
 		
 		String change = "";
 		
+		//Determine change needed
 		BigDecimal changeNeeded = amountGiven.subtract(totalDue);
 		
+		//Create a hash set to keep track of used change value indexes
 		HashSet<Integer> indexSet = new HashSet<>();
 		
+		//While all denominations haven't been used and change needed isn't zero
 		while(indexSet.size()!=changeValues.length && changeNeeded.compareTo(zero)!= 0) {
 			
+			//generate random index
 			int randomIndex = generateRandomIndex();
 			
+			//check to see if random index already used; if so re-calculate till find one
 			while(indexSet.contains(randomIndex)) {
 				randomIndex = generateRandomIndex();
 			}
 			
 			int numberOfChange = 0;
 			
+			//subtract change till at zero or less than zero
 			while((changeNeeded.subtract(changeValues[randomIndex]).compareTo(zero)==1)||
 					(changeNeeded.subtract(changeValues[randomIndex]).compareTo(zero)==0)) {
 				
@@ -71,6 +79,7 @@ public class ChangeProcessingService {
 				changeNeeded = changeNeeded.subtract(changeValues[randomIndex]);
 			}
 			
+			//create change strings
 			if(numberOfChange == 1) {
 				change += numberOfChange + " " + changeStringSingle[randomIndex] + ",";
 			}
@@ -78,6 +87,7 @@ public class ChangeProcessingService {
 				change += numberOfChange + " " + changeStringPlural[randomIndex] + ",";
 			}	
 			
+			//Add index to set
 			indexSet.add(randomIndex);
 		}
 
