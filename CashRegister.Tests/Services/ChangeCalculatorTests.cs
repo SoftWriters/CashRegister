332ced942da07c8acadfd0659ccd2f6e46212cbf
Aaron Cooper <mr.aaroncooper@gmail.com>
2021-09-26 22:02:09 -0400

    Add changebuiler interface, unit tests for change calculator

--- CashRegister.Tests/Services/ChangeCalculatorTests.cs
+++ CashRegister.Tests/Services/ChangeCalculatorTests.cs
@@ -1,13 +1,24 @@
+using Moq;
 using CashRegister.Exceptions;
 using CashRegister.Services;
 using NUnit.Framework;
+using CashRegister.Services.Interfaces;
 
 namespace CashRegister.Tests.Services
 {
     [TestFixture]
     public class ChangeCalculatorTests
     {
-        protected ChangeCalculator uut = new ChangeCalculator();
+        private Mock<IRandomChangeCalculator> mockRandomChangeCalculator;
+        private Mock<IChangeBuilder> mockChangeBuilder;
+        protected ChangeCalculator uut;
+
+        public ChangeCalculatorTests()
+        {
+            mockRandomChangeCalculator = new Mock<IRandomChangeCalculator>();
+            mockChangeBuilder = new Mock<IChangeBuilder>();
+            uut = new ChangeCalculator(mockRandomChangeCalculator.Object, mockChangeBuilder.Object);
+        }
 
         [Test]
         public void CalculateChange_Should_ReturnAdecimal()
@@ -54,6 +65,50 @@ namespace CashRegister.Tests.Services
             Assert.AreEqual(expectedExceptionMessage, thrownException.Message);
         }
 
+        [Test]
+        public void DetermineChange_Should_ReturnAString()
+        {
+            var costDue = 3;
 
+            mockChangeBuilder.Setup(cb => cb.BuildChangeString(It.IsAny<decimal>(), It.IsAny<decimal>(), It.IsAny<decimal>(), It.IsAny<decimal>(), It.IsAny<decimal>()))
+                .Returns("");
+
+            var results = uut.DetermineChange(costDue);
+            Assert.IsInstanceOf<string>(results);
+        }
+
+        [Test]
+        public void DetermineChange_Should_CallChangeBuilderWithExpectedArguments()
+        {
+            var costDue = 2.67m;
+            var expectedDollars = 2;
+            var expectedQuarters = 2;
+            var expectedDimes = 1;
+            var expectedNickels = 1;
+            var expectedPennies = 2;
+
+            var results = uut.DetermineChange(costDue);
+
+            mockChangeBuilder.Verify(cb => cb.BuildChangeString(expectedDollars, expectedQuarters, expectedDimes, expectedNickels, expectedPennies), Times.Once);
+        }
+
+        [Test]
+        public void DetermineChange_Should_ReturnTheExpectedResult()
+        {
+            var costDue = 2.67m;
+            var expectedDollars = 2;
+            var expectedQuarters = 2;
+            var expectedDimes = 1;
+            var expectedNickels = 1;
+            var expectedPennies = 2;
+
+            var expectedResults = "2 dollars, 2 quarters, 1 dime, 1 nickel, 2 pennies";
+
+            mockChangeBuilder.Setup(cb => cb.BuildChangeString(expectedDollars, expectedQuarters, expectedDimes, expectedNickels, expectedPennies))
+                .Returns(expectedResults);
+
+            var results = uut.DetermineChange(costDue);
+            Assert.AreEqual(expectedResults, results);
+        }
     }
 }
